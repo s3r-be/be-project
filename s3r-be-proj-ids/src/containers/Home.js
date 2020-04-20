7987e7837054a7bef8a8b81b868f285295b06be1
rhish9h <rhish9h@gmail.com>
2020-04-21 00:13:29 +0530

    updated home page

--- s3r-be-proj-ids/src/containers/Home.js
+++ s3r-be-proj-ids/src/containers/Home.js
@@ -1,17 +1,28 @@
 import PropTypes from "prop-types";
+
 import React, { Component } from "react";
+
+// import ReadMoreReact from 'read-more-react';
+
 import {
   Button,
   Container,
   Divider,
   Grid,
   Header,
-  Image,
+  // Image,
   Responsive,
   Segment,
   Sidebar,
   Visibility
 } from "semantic-ui-react";
+
+import "./style.css";
+import ddos from '../Images/Home/ddos.jpg';
+import dtype from '../Images/Home/dtype.jpg';
+import mitm from '../Images/Home/mitm.jpeg';
+import scan from '../Images/Home/scan.jpg';
+import wrong from '../Images/Home/wrong.jpeg';
 
 const getWidth = () => {
   const isSSR = typeof window === "undefined";
@@ -80,41 +91,55 @@ const ResponsiveContainer = ({ children }) => (
 ResponsiveContainer.propTypes = {
   children: PropTypes.node
 };
-
+/*minimumLength = 80
+idealLength = 100
+maxLength = 200
+yourTextHere = 'xyz'*/
+// function ReadMore() {
+//   return (
+//     <ReadMoreReact text={'xyz'}
+//       min={80}
+//       ideal={100}
+//       max={200}
+//       readMoreText="click here to read more" />
+//   );
+// }
 const HomepageLayout = () => (
   <ResponsiveContainer>
     <Segment style={{ padding: "8em 0em" }} vertical>
       <Grid container stackable verticalAlign="middle">
         <Grid.Row>
           <Grid.Column width={8}>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              We Help Companies and Companions
+            <Header as="h3" style={{ fontSize: "2em", color: "white" }}>
+              Intrusion Detection System
             </Header>
-            <p style={{ fontSize: "1.33em" }}>
-              We can give your company superpowers to do things that they never
-              thought possible. Let us delight your customers and empower your
-              needs... through pure data analytics.
+            <p style={{ fontSize: "1.33em", color: "#cecece" }}>
+              An intrusion detection system is one such system that can be a building block in
+              making the IOT network as secure as possible,that passively monitors the data exchange
+              in the network and of the network with externals entities and looks for malicious
+              activities that can be classified as an intrusion or attack.
             </p>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              We Make Bananas That Can Dance
+            <Header as="h3" style={{ fontSize: "2em", color: "white" }}>
+              We care about Protection
             </Header>
-            <p style={{ fontSize: "1.33em" }}>
-              Yes that's right, you thought it was the stuff of dreams, but even
-              bananas can be bioengineered.
+            <p style={{ fontSize: "1.33em", color: "#cecece" }}>
+              Includes IOT networks like CCTVs,smoke detectors,fire alarms,speaker systems,thermostats,vending machines.etc
             </p>
           </Grid.Column>
           <Grid.Column floated="right" width={6}>
-            <Image
-              bordered
-              rounded
-              size="large"
-              src="/images/wireframe/white-image.png"
+
+            <img alt='idk'
+            /*  src={quan}
+
+              width="2000"
+              height="200"*/
+
             />
           </Grid.Column>
         </Grid.Row>
         <Grid.Row>
           <Grid.Column textAlign="center">
-            <Button size="huge">Check Them Out</Button>
+            <Button size="huge">Check Out The <span color="red">ATTACKS</span> detected</Button>
           </Grid.Column>
         </Grid.Row>
       </Grid>
@@ -122,61 +147,96 @@ const HomepageLayout = () => (
     <Segment style={{ padding: "0em" }} vertical>
       <Grid celled="internally" columns="equal" stackable>
         <Grid.Row textAlign="center">
-          <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              "What a Company"
-            </Header>
+          <Grid.Column style={{ paddingBottom: "5em", paddingTop: "1em" }}>
             <p style={{ fontSize: "1.33em" }}>
-              That is what they all say about us
+
+              <img alt='ddos'
+                src={ddos}
+
+                width="500"
+                height="200"
+
+              />
+            </p>
+            <p>
+              <img alt='dtype'
+                src={dtype}
+                width="500"
+                height="200"
+              />
+            </p>
+            <p style={{ fontSize: "1.33em" }}>
+            </p>
+            <p style={{ fontSize: "1.33em" }}>
+
+              <img alt='mitm'
+                src={mitm}
+                width="500"
+                height="200"
+              />
             </p>
           </Grid.Column>
-          <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              "I shouldn't have gone with their competitor."
-            </Header>
+          <Grid.Column style={{ paddingBottom: "5em", paddingTop: "1em" }}>
+
             <p style={{ fontSize: "1.33em" }}>
-              <Image avatar src="/images/avatar/large/nan.jpg" />
-              <b>Nan</b> Chief Fun Officer Acme Toys
+              <img alt='scan'
+                src={scan}
+                width="500"
+                height="200"
+              />
+            </p>
+            <p style={{ fontSize: "1.33em" }}>
+              <img alt='wrong'
+                src={wrong}
+                width="500"
+                height="200"
+              />
             </p>
           </Grid.Column>
         </Grid.Row>
+
       </Grid>
     </Segment>
     <Segment style={{ padding: "8em 0em" }} vertical>
       <Container text>
-        <Header as="h3" style={{ fontSize: "2em" }}>
-          Breaking The Grid, Grabs Your Attention
+        <Header as="h3" style={{ fontSize: "2em", color: "white" }}>
+          Overall working of the System
         </Header>
-        <p style={{ fontSize: "1.33em" }}>
-          Instead of focusing on content creation and hard work, we have learned
-          how to master the art of doing nothing by providing massive amounts of
-          whitespace and generic content that can seem massive, monolithic and
-          worth your attention.
+        <p style={{ fontSize: "1.33em", color: "#cecece" }}>
+          This IDS is designed to run in the background of an IoT network, where distance data is processed and sent from the node (combination of ultrasonic sensor, Arduino, Nodemcu) to the apache server. The network traffic is monitored using Tshark and dumped into a csv file. The python script in the backend reads this csv file, preprocesses it and feeds it to the ML model. If an intrusion is detected, it is notified to the user on the front end.
+          The ML model Random Forest is used as its accuracy was proven to be better than any other model that was tested. The model was trained using the data collected from the IoT network traffic.
         </p>
-        <Button as="a" size="large">
-          Read More
-        </Button>
+
         <Divider
           as="h4"
           className="header"
           horizontal
           style={{ margin: "3em 0em", textTransform: "uppercase" }}
         >
-          <a href="#">Case Studies</a>
+
         </Divider>
-        <Header as="h3" style={{ fontSize: "2em" }}>
-          Did We Tell You About Our Bananas?
+        <Header as="h3" style={{ fontSize: "2em", color: "white" }}>
+          To know more about the Attacks
         </Header>
-        <p style={{ fontSize: "1.33em" }}>
-          Yes I know you probably disregarded the earlier boasts as non-sequitur
-          filler content, but it's really true. It took years of gene splicing
-          and combinatory DNA research, but our bananas can really dance.
+        <p style={{ fontSize: "1.33em", color: "#cecece" }}>
+
+          DDOS-A distributed denial-of-service (DDoS) attack is a malicious attempt to disrupt normal traffic of a targeted server, service or network by overwhelming the target or its surrounding infrastructure with a flood of Internet traffic
+          <br />
+          SCAN-A port scan is an attack that sends client requests to a range of server port addresses on a host, with the goal of finding an active port and exploiting a known vulnerability of that service
+          <br />
+          DATA TYPE PROBING-Probing is a type of attack in which the intruder scans network devices to determine weakness in topology design or some opened ports and then use them in the future for illegal access to personal information
+          <br />
+          WRONG SETUP-It is a type of attack where attackers attempt to remove the connection or manipulate the connection of the IOT network
+          <br />
+          MITM-It is an attack where the attacker secretly relays and possibly alters the communications between two parties who believe that they are directly communicating with each other
+          <br />
+
+
         </p>
-        <Button as="a" size="large">
-          I'm Still Quite Interested
-        </Button>
+
       </Container>
     </Segment>
   </ResponsiveContainer>
 );
 export default HomepageLayout;
+
