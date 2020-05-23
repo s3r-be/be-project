a573964f06d5692e7efa13c68bad65c7143b9e23
rhish9h <rhish9h@gmail.com>
2020-05-23 18:49:14 +0530

    changed server.php to maintain 10 values in datastorage.txt

--- ids/server.php
+++ ids/server.php
@@ -5,12 +5,15 @@ $filename = "datastorage.txt";
 $fileStatus=file_put_contents($filename,$fileContent,FILE_APPEND);
 if($fileStatus != false)
 {
-                echo  "SUCCESS. Data written in file.";
-		$file = file($filename);
-  		unset($file[0]);
-  		file_put_contents($filename, $file);
+	echo  "SUCCESS. Data written in file.";
+	$file = file($filename);
+	$dataCount = count($file);
+	for ($i = 0; $i < $dataCount-10; $i++) {
+		unset($file[$i]);
+	}
+  	file_put_contents($filename, $file);
 }
 else
 {
-                echo  "FAIL. Could not connect to file.";
+	echo  "FAIL. Could not connect to file.";
 }
